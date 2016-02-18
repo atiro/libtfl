9fd952024a70b54716a461f4aabff515b4b4f9c8
Richard Palmer <git@tiro.org.uk>
2016-02-18 22:58:35 +0000

    Starting to think how this might work. Hardcoded example

--- libtfl.py
+++ libtfl.py
@@ -1,4 +1,50 @@
 import requests
-import json
 
-# https://api.tfl.gov.uk/StopPoint/490010540S/arrivals
+TFL_API_APP_ID = ""
+TFL_API_APP_KEY = ""
+MY_STOP = ""
+
+class TFLBase:
+    pass
+
+class Stop(TFLBase):
+    stop_point = None
+    stop_time = None
+
+    def show_arrivals():
+        pass
+
+class Transport(TFLBase):
+    pass
+
+class Bus(Transport):
+    route = None
+    stop = None
+    json = None
+
+    route_stops = []
+    next_stop = None
+
+    def __init__(self, route=None, stop=None):
+        self.route = route
+        self.stop = stop
+        print("Created route", self.route)
+
+    def update(self):
+        req = requests.get("https://api.tfl.gov.uk/StopPoint/%s/Arrivals?app_id=%s&app_key=%s")
+        self.json = req.json()
+        self.next_stop = self.json[0]['timeToStation']
+
+    def status(self):
+        pass
+
+    def __str__(self):
+        return self.route
+
+r22 = Bus(route="R22", stop=MY_STOP)
+
+r22.update()
+
+print("Next bus is in approximately %d seconds" % r22.next_stop)
+
+print("Route is", r22)
