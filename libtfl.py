87e9aff80c626eb1cfe582ab112fd7e557dfd8bd
Richard Palmer <git@tiro.org.uk>
2016-02-21 16:34:25 +0000

    Rough outline code structure working

--- libtfl.py
+++ libtfl.py
@@ -1,26 +1,57 @@
 import requests
+import os
 
 TFL_API_URL = "https://api.tfl.gov.uk/"
 
-TFL_API_APP_ID = ""
-TFL_API_APP_KEY = ""
-MY_STOP = ""
+TFL_API_APP_ID = os.environ.get('TFL_API_APP_ID')
+TFL_API_APP_KEY = os.environ.get('TFL_API_APP_KEY')
+BUS_STOP = os.getenv('MY_STOP', '490013767A')  # Default Charing Cross Bus Stop
+
 
 class TFLBase:
-    type = None
-    action = None
+    query = None
+    query_id = None
+    filtered = None
+    filtered_id = None
+    subfilter = None
 
     def update():
         pass
         # construct url based on api_url, type, action, and keys
+# return response
+
+    def api(self):
+        url = TFL_API_URL + self.query + '/'
+        if self.query_id:
+            url += self.query_id + '/'
+        if self.filtered:
+            url += self.filtered + '/'
+        if self.filtered_id:
+            url += self.filtered_id + '/'
+        # TODO handle param args
+
+        try:
+          r = requests.get(url)
+        except:
+          print("Failed to get data for URL: ", url)
+
+        print("Returning data for url: ", url)
+        # TODO store last synced data time
+        return r.json()
 
 
 class Stop(TFLBase):
     stop_point = None
     stop_time = None
 
-    def show_arrivals():
-        pass
+    def __init__(self):
+        print("In Stop init")
+        self.query = "StopPoint"
+
+
+    def arrivals(self):
+        self.filtered = "Arrivals"
+        self.data = self.api()
 
 
 class Transport(TFLBase):
@@ -28,28 +59,36 @@ class Transport(TFLBase):
 
 # Modes of Transport
 
+
 class Bus(Transport):
     pass
+
 
 class Tube(Transport):
     pass
 
+
 class Bike(Transport):
     pass
+
 
 class Train(Transport):
     pass
 
+
 class CableCar(Transport):
     pass
 
+
 class DLR(Transport):
     pass
+
 
 class Tram(Transport):
     pass
 
 # Transport egress
+
 
 class Station(Stop):
     pass
@@ -70,23 +109,27 @@ class BusStop(Stop):
     next_stop = None
 
     def __init__(self, stop=None, route=None):
-        self.stop = stop
+        print("In BusStop init")
+        self.query_id = stop
         self.route = route
-        print("Created route", self.route)
-
-    def update(self):
-        self.action = "Arrivals"
-        req = requests.get("https://api.tfl.gov.uk/StopPoint/%s/Arrivals?app_id=%s&app_key=%s")
-        self.json = req.json()
-        self.next_stop = self.json[0]['timeToStation']
+        super().__init__()
 
     def status(self):
         pass
+
+    def next_bus(self, route = None):
+        if route:
+            pass
+        else:
+            return self.data[0]['timeToStation']
 
     def __str__(self):
         return self.route
 
 # Lines/Routes
+
+class Location(TFLBase):
+    pass
 
 class Place(Location):
     pass
@@ -111,10 +154,10 @@ class Journey(TFLBase):
 
 
 
-r22 = BusStop(route="R22", stop=MY_STOP)
+#stop  = BusStop(stop=BUS_STOP, route="R22")
+stop  = BusStop(stop=BUS_STOP)
+stop.arrivals()
 
-r22.update()
+print("Next bus is in approximately %d seconds" % stop.next_bus())
 
-print("Next bus is in approximately %d seconds" % r22.next_stop)
-
-print("Route is", r22)
+#print("Route is", r22)
