73906e66726ef1403193844b611c9fcb20ae943e
Richard Palmer <git@tiro.org.uk>
2016-02-21 19:36:03 +0000

    Starting to flesh out StopPoint API calls

--- libtfl.py
+++ libtfl.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from collections import OrderedDict
 
 TFL_API_URL = "https://api.tfl.gov.uk/"
 
@@ -15,12 +16,14 @@ class TFLBase:
     filtered_id = None
     subfilter = None
 
+    data = None
+
     def update():
         pass
         # construct url based on api_url, type, action, and keys
 # return response
 
-    def api(self):
+    def __api__(self):
         url = TFL_API_URL + self.query + '/'
         if self.query_id:
             url += self.query_id + '/'
@@ -31,13 +34,13 @@ class TFLBase:
         # TODO handle param args
 
         try:
-          r = requests.get(url)
+            r = requests.get(url)
         except:
-          print("Failed to get data for URL: ", url)
+            print("Failed to get data for URL: ", url)
 
         print("Returning data for url: ", url)
         # TODO store last synced data time
-        return r.json()
+        self.data = r.json()
 
 
 class Stop(TFLBase):
@@ -48,10 +51,65 @@ class Stop(TFLBase):
         print("In Stop init")
         self.query = "StopPoint"
 
-
     def arrivals(self):
         self.filtered = "Arrivals"
         self.data = self.api()
+
+    def within(self, criteria=None):
+        pass
+
+    def bbox(self, bbox=None):
+        pass
+
+    def can_reach_on_line(self, lineId=None):
+        pass
+
+    def direction_to(self, stoppoint=None):
+        pass
+
+    def disruption(self, ids=None, start_date=None, end_date=None):
+        self.filtered = "Disruption"
+
+        if ids is not None:
+            self.query_id = ids
+
+        # TODO filter by date
+
+        self.__api__()
+
+        return self.data
+
+    def fareTo(self, stop_point=None):
+        self.filtered = "FareTo"
+        self.filtered_id = stop_point
+
+        self.__api__()
+
+        return self.data
+
+    def meta(self):
+        pass
+
+    def mode(self):
+        pass
+
+    def routes(self, serviceTypes="Regular"):
+        self.filtered = "Route"
+
+        self.__api__()
+
+        # TODO Turn map line locations into something useful
+
+        return self.data
+
+    def query(self):
+        pass
+
+    def service_types(self):
+        pass
+
+    def type(self):
+        pass
 
 
 class Transport(TFLBase):
@@ -61,7 +119,27 @@ class Transport(TFLBase):
 
 
 class Bus(Transport):
-    pass
+    data = None
+
+    vehicleId = None
+    lineName = None
+    destinationName = None
+    currentLocation = None
+    direction = None
+    expectedArrival = None
+    timeToStation = None
+
+    def __init__(self):
+        pass
+
+    def __data__(self, data):
+        self.data = data
+
+    def lineName(self):
+        return self.data['lineName']
+
+    def timeToStation(self):
+        return self.data['timeToStation']
 
 
 class Tube(Transport):
@@ -101,7 +179,14 @@ class BikePoint(Stop):
 
 
 class BusStop(Stop):
-    route = None
+    stop_routes = {}
+    stop_vehicles = {}
+
+    queue = []
+
+    next_bus = None
+    next_buses = {}
+
     stop = None
     json = None
 
@@ -114,50 +199,102 @@ class BusStop(Stop):
         self.route = route
         super().__init__()
 
+    def __arrivals(self):
+        self.filtered = "Arrivals"
+
+        super().__api__()
+
+        for bus in self.data:
+            self.stop_routes[bus['lineName']] = 1
+            self.stop_vehicles[bus['vehicleId']] = 1
+
+        self.queue = sorted(self.data, key=lambda b: int(b['timeToStation']))
+
     def status(self):
         pass
 
-    def next_bus(self, route = None):
+    def next_bus_time(self, route=None):
+        if self.data is None:
+            self.__arrivals()
+
         if route:
-            pass
+            for bus in self.queue:
+                if bus['lineName'] == route:
+                    return bus['timeToStation']
         else:
-            return self.data[0]['timeToStation']
+            if self.queue is not None:
+                return self.queue[0]['timeToStation']
+
+        return None
+
+    def next_bus(self, route=None):
+        nextbus = None
+
+        if self.data is None:
+            self.__arrivals()
+
+        if route:
+            for bus in self.queue:
+                if bus['lineName'] == route:
+                    nextbus = Bus()
+                    nextbus.__data__(bus)
+        else:
+            if self.queue is not None:
+                nextbus = Bus()
+                nextbus.__data__(self.queue[0])
+
+        return nextbus
+
+    def route(self):
+        super().route()
 
     def __str__(self):
-        return self.route
+        return self.query_id
 
 # Lines/Routes
+
 
 class Location(TFLBase):
     pass
 
+
 class Place(Location):
     pass
+
 
 class Line(Location):
     pass
 
+
 class Route(Location):
     pass
+
 
 class Road(Location):
     pass
 
 # Search
 
+
 class Search(TFLBase):
     pass
+
 
 class Journey(TFLBase):
     """ Journey Planner Query """
     pass
 
 
+# stop  = BusStop(stop=BUS_STOP, route="R22")
+stop = BusStop(stop=BUS_STOP)
+# stop.arrivals()
 
-#stop  = BusStop(stop=BUS_STOP, route="R22")
-stop  = BusStop(stop=BUS_STOP)
-stop.arrivals()
+print("Next bus arrives in approximately %d seconds" % stop.next_bus_time())
 
-print("Next bus is in approximately %d seconds" % stop.next_bus())
+print("Stop is served by routes: %s" % stop.routes())
 
-#print("Route is", r22)
+bus = stop.next_bus()
+
+print("Next bus is on route %s" % bus.lineName())
+
+# print("Route is", r22)
