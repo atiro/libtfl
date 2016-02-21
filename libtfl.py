80c85ee78b5c25eb90ddeb2849d625673949e400
Richard Palmer <git@tiro.org.uk>
2016-02-21 14:43:34 +0000

    Trying to handle all the API queries

--- libtfl.py
+++ libtfl.py
@@ -1,11 +1,19 @@
 import requests
 
+TFL_API_URL = "https://api.tfl.gov.uk/"
+
 TFL_API_APP_ID = ""
 TFL_API_APP_KEY = ""
 MY_STOP = ""
 
 class TFLBase:
-    pass
+    type = None
+    action = None
+
+    def update():
+        pass
+        # construct url based on api_url, type, action, and keys
+
 
 class Stop(TFLBase):
     stop_point = None
@@ -14,10 +22,46 @@ class Stop(TFLBase):
     def show_arrivals():
         pass
 
+
 class Transport(TFLBase):
     pass
 
+# Modes of Transport
+
 class Bus(Transport):
+    pass
+
+class Tube(Transport):
+    pass
+
+class Bike(Transport):
+    pass
+
+class Train(Transport):
+    pass
+
+class CableCar(Transport):
+    pass
+
+class DLR(Transport):
+    pass
+
+class Tram(Transport):
+    pass
+
+# Transport egress
+
+class Station(Stop):
+    pass
+
+
+class BikePoint(Stop):
+
+    def __init__(self, point=None, bbox=None, locus=None, name=None):
+        pass
+
+
+class BusStop(Stop):
     route = None
     stop = None
     json = None
@@ -25,12 +69,13 @@ class Bus(Transport):
     route_stops = []
     next_stop = None
 
-    def __init__(self, route=None, stop=None):
-        self.route = route
+    def __init__(self, stop=None, route=None):
         self.stop = stop
+        self.route = route
         print("Created route", self.route)
 
     def update(self):
+        self.action = "Arrivals"
         req = requests.get("https://api.tfl.gov.uk/StopPoint/%s/Arrivals?app_id=%s&app_key=%s")
         self.json = req.json()
         self.next_stop = self.json[0]['timeToStation']
@@ -41,7 +86,32 @@ class Bus(Transport):
     def __str__(self):
         return self.route
 
-r22 = Bus(route="R22", stop=MY_STOP)
+# Lines/Routes
+
+class Place(Location):
+    pass
+
+class Line(Location):
+    pass
+
+class Route(Location):
+    pass
+
+class Road(Location):
+    pass
+
+# Search
+
+class Search(TFLBase):
+    pass
+
+class Journey(TFLBase):
+    """ Journey Planner Query """
+    pass
+
+
+
+r22 = BusStop(route="R22", stop=MY_STOP)
 
 r22.update()
 
